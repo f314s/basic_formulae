45c8eb69b7daab83729111445a5da42d410b4d06
Faris Khan <98594863+f314s@users.noreply.github.com>
2023-07-13 23:18:49 +0600

    Create (a-b)_whole_square.cpp

---
+++ (a-b)_whole_square.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double num1, num2;
+    char repeat_calculation = 'y';
+
+    while (repeat_calculation == 'y' || repeat_calculation == 'Y'){
+
+    cout << "enter the first number :" << endl;
+    cin >> num1;
+    cout << "enter the second :" << endl;
+    cin >> num2;
+
+    cout << "(" << num1 << "-" << num2 << ")^2" << endl;
+    cout << num1 << "^2-2*" << num1 << "*" << num2 << "+" << num2 << "^2" << endl;
+    cout << num1 * num1 << "-" << 2* num1 * num2 << "+" << num2 * num2 << endl;
+    cout << num1 * num1 - 2 * num1 * num2 + num2 * num2 << endl;
+    cout << "calculate again y/n" << endl;
+    cin >> repeat_calculation;
+    }
+    return 0;
+}
